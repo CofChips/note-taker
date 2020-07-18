103ec1c170a80e53989d88442787c6cbf6439541
CofChips <qiwei.mod@gmail.com>
2020-07-17 17:12:17 -0700

    added post route that captures new tasks writes a new task list to the db.json and then returns this list

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { send } = require("process");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-let testData = [{"title":"Test Title","text":"Test text"}];
+let testData = [{ "title": "Test Title", "text": "Test text" }];
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -19,11 +19,37 @@ app.get("/notes", function (req, res) {
 })
 
 app.get("/api/notes", function (req, res) {
-    fs.readFile("db/db.json", "utf-8", function (error, data) {
-        console.log("this is JSON:" + JSON.parse(data));
+    fs.readFile("db/db.json", function (error, data) {
         let sendData = JSON.parse(data);
         res.json(sendData);
     })
+})
+
+app.post("/api/notes", function (req, res) {
+    const newArray = [];
+    let newNote = req.body;
+    console.log(newNote);
+    newArray.push(newNote);
+    fs.readFile("db/db.json", function (error, data) {
+        let temp = JSON.parse(data);
+        for (let i = 0; i < temp.length; i++) {
+            newArray.push(temp[i])
+        }
+        console.log(newArray);
+        fs.writeFile("db/db.json", JSON.stringify(newArray), function (error, data) {
+            if (error) throw error;
+            console.log("Check the file")
+        })
+        fs.readFile("db/db.json", function (error, data) {
+            let sendData = JSON.parse(data);
+            res.json(sendData);
+        })
+
+    })
+
+
+    console.log(newArray)
+
 })
 
 app.get("*", function (req, res) {
@@ -38,3 +64,4 @@ app.listen(PORT, function (req, res) {
 
 
 
+// [{"title":"Test Title","text":"Test text"}]
